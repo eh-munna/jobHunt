4e5daca88b15b903a5e9ef1f0ecc62e2f4121049
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-04-10 17:44:53 +0200

    job details card implemented

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
-import Salary from '../Salary/Salary';
+import Button from '../Button/Button';
 
 const JobDetails = () => {
   const { jobs } = useLoaderData();
@@ -13,7 +13,7 @@ const JobDetails = () => {
     //   .then((data) => setDetails(data));
     const targetJob = jobs.find((job) => job.id == da.id);
     setDetails(targetJob);
-  }, []);
+  }, [jobs]);
   const {
     company_name,
     job_description,
@@ -21,12 +21,14 @@ const JobDetails = () => {
     educational_requirements,
     experiences,
     job_title,
-    contact_information,
+    phone,
+    email,
     salary,
     location,
+    address,
   } = details;
   return (
-    <div className="container mx-auto py-4 md:py-0 px-6 md:px-12 grid grid-cols-1 md:grid-cols-3">
+    <div className="container mx-auto py-4 md:py-0 px-6 md:px-12 grid grid-cols-1 md:grid-cols-3 my-8 md:my-16">
       <div className="md:col-span-2">
         <div className="leading-7">
           <h3 className="leading-7 text-[#1A1919] font-semibold">
@@ -54,56 +56,139 @@ const JobDetails = () => {
           <p className="font-normal">{experiences}</p>
         </div>
       </div>
-      <div className="p-7 rounded-lg md:col-span-1 bg-[#edeeff]">
-        <div>
-          <h5 className="text-[#1A1919] text-lg capitalize font-semibold">
-            job details
-          </h5>
-          <hr />
+      <div className=" md:col-span-1 ">
+        <div className="p-7 mb-4 rounded-lg bg-[#edeeff]">
+          <div className="mb-5">
+            <h5 className="text-[#1A1919] text-lg capitalize font-semibold">
+              job details
+            </h5>
+          </div>
+          <hr className="border-[#d8cbff]" />
+          <div className="space-y-3 py-4">
+            <div>
+              <h5 className="text-[#474747] text-base md:text-lg font-semibold inline-flex items-center gap-2">
+                <span className="text-[#7E90FE]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </span>
+                Salary :{' '}
+                <span className="text-[#757575]">{salary} (Per Month)</span>
+              </h5>
+            </div>
+            <div>
+              <h5 className="text-[#474747] text-base md:text-lg font-semibold inline-flex items-center gap-2">
+                <span className="text-[#7E90FE]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                    />
+                  </svg>
+                </span>
+                Job Title : <span className="text-[#757575]">{job_title}</span>
+              </h5>
+            </div>
+          </div>
+          <div className="mb-5">
+            <h5 className="text-[#1A1919] text-lg capitalize font-semibold">
+              Contact Information
+            </h5>
+          </div>
+          <hr className="border-[#d8cbff]" />
+          <div className="space-y-3 py-4">
+            <div>
+              <h5 className="text-[#474747] text-base md:text-lg font-semibold inline-flex items-center gap-2">
+                <span className="text-[#7E90FE]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+                    />
+                  </svg>
+                </span>
+                Phone : <span className="text-[#757575]">{phone}</span>
+              </h5>
+            </div>
+            <div>
+              <h5 className="text-[#474747] text-base md:text-lg font-semibold inline-flex items-center gap-2">
+                <span className="text-[#7E90FE]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                    />
+                  </svg>
+                </span>
+                Email : <span className="text-[#757575]">{email}</span>
+              </h5>
+            </div>
+            <div>
+              <h5 className="text-[#474747] text-base md:text-lg font-semibold inline-flex items-center gap-2">
+                <span className="text-[#7E90FE]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                    />
+                  </svg>
+                </span>
+                Address : <span className="text-[#757575]">{address}</span>
+              </h5>
+            </div>
+          </div>
         </div>
-        <div>
-          <h5 className="text-[#474747] text-base md:text-lg font-semibold inline-flex items-center gap-2">
-            <span className="text-[#7E90FE]">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </span>
-            Salary :{' '}
-            <span className="text-[#757575]">{salary} (Per Month)</span>
-          </h5>
-        </div>
-        <div>
-          <h5 className="text-[#474747] text-base md:text-lg font-semibold inline-flex items-center gap-2">
-            <span className="text-[#7E90FE]">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
-                />
-              </svg>
-            </span>
-            Job Title : <span className="text-[#757575]">{job_title}</span>
-          </h5>
-        </div>
+        <button className=" w-full capitalize rounded-md p-2 md:p-4 text-[#ffffff] leading-4 font-bold text-lg bg-gradient-to-r from-[#7E90FE] from-0% to-[#9873FF] to-100%">
+          Apply now
+        </button>
       </div>
     </div>
   );
