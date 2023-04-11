122c0d5e7546f1b09c4e62df5efedfe1e4c684b1
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-04-11 23:54:56 +0200

    data filtering is working

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -3,9 +3,45 @@ import { Link, useLoaderData } from 'react-router-dom';
 import SavedJobs from '../SavedJobs/SavedJobs';
 import vector from '../../images/Vector.png';
 import vector1 from '../../images/Vector-1.png';
+import Button from '../Button/Button';
 
 const AppliedJobs = () => {
   const storedJobs = useLoaderData();
+  const [allJobs, setAllJobs] = useState(storedJobs);
+  // console.log(allJobs);
+  // const [isFilter, setIsFilter] = useState(false);
+  const [remote, setRemote] = useState([]);
+  const [onsite, setOnsite] = useState([]);
+
+  // filter remote job
+
+  useEffect(() => {
+    const remoteJob = storedJobs.filter(
+      (job) => job.remote_or_onsite == 'Remote'
+    );
+    setRemote(remoteJob);
+  }, [allJobs]);
+
+  const remoteData = () => {
+    // setIsFilter(true);
+    setAllJobs(remote);
+  };
+
+  // filter onsite job
+
+  useEffect(() => {
+    const onsiteJob = storedJobs.filter(
+      (job) => job.remote_or_onsite == 'Onsite'
+    );
+    setOnsite(onsiteJob);
+  }, [allJobs]);
+
+  const onsiteData = () => {
+    // setIsFilter(true);
+    setAllJobs(onsite);
+  };
+
+  // console.log(remote);
 
   return (
     <div>
@@ -16,9 +52,32 @@ const AppliedJobs = () => {
         <img className="absolute bottom-0" src={vector} alt="" />
       </div>
       <img className="absolute right-0 top-0" src={vector1} alt="" />
-      {storedJobs.map((job) => (
-        <SavedJobs key={job.id} job={job}></SavedJobs>
-      ))}
+      <div className="container mx-auto p2-4 md:py-0 px-6 md:px-12 my-4 md:my-16 gap-10">
+        <div className="flex justify-center md:justify-end gap-2">
+          <span onClick={remoteData}>
+            <Button>Show Remote</Button>
+          </span>
+          <span onClick={onsiteData}>
+            <Button>Show Onsite</Button>
+          </span>
+        </div>
+      </div>
+      <div>
+        {/* {isFilter
+          ? remote.map((job) => <SavedJobs key={job.id} job={job}></SavedJobs>)
+          : allJobs.map((job) => (
+              <SavedJobs key={job.id} job={job}></SavedJobs>
+            ))} */}
+
+        {/* {isFilter
+          ? onsite.map((job) => <SavedJobs key={job.id} job={job}></SavedJobs>)
+          : allJobs.map((job) => (
+              <SavedJobs key={job.id} job={job}></SavedJobs>
+            ))} */}
+        {allJobs.map((job) => (
+          <SavedJobs key={job.id} job={job}></SavedJobs>
+        ))}
+      </div>
     </div>
   );
 };
